a7204fad1eeac82facb1f7248714977163fe1374
Spakir <danntyl@mail.ru>
2025-04-28 17:49:16 +0300

    Интерфейс для SoftServiceImpl

---
+++ src/main/java/com/example/testing/softservice/service/SoftService.java
@@ -0,0 +1,8 @@
+package com.example.testing.softservice.service;
+
+import com.example.testing.softservice.model.RequestData;
+
+public interface SoftService {
+
+    Long findMinimumElement(RequestData requestBody);
+}
