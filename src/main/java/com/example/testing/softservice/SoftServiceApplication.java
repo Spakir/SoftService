dcc82a0e7025d675208d4bbc697fc0d8825a4f3e
Spakir <danntyl@mail.ru>
2025-04-28 17:55:56 +0300

    SoftServiceApplication

---
+++ src/main/java/com/example/testing/softservice/SoftServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.testing.softservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftServiceApplication.class, args);
+    }
+
+}
