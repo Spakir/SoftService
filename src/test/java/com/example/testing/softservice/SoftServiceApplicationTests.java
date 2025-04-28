14ea61264813f62c1fd15f060e8966b174cebb25
Spakir <danntyl@mail.ru>
2025-04-28 17:55:44 +0300

    SoftServiceApplicationTests

---
+++ src/test/java/com/example/testing/softservice/SoftServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testing.softservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
